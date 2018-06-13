96d07f37177e6bb384e75a87990e5acc0203d3e5
Leva <levon.oganesov@mail.ru>
2018-06-13 07:11:04 +0300

    work 90%

--- js/outlineStrokePng.js
+++ js/outlineStrokePng.js
@@ -8,76 +8,55 @@
  * @Version: 1.0.0
  * -----------------------
  */
+var ImageOutline = (function () {
 
-function ImageOutline(param) {
-    this.img = new Image();
-    this.img.src = param.img.src;
+    var imageOutline;
 
-    this.imgW = param.img.W;
-    this.imgH = param.img.H;
-    this.imgX = param.img.X;
-    this.imgY = param.img.Y;
-    this.findAlpha = param.img.findAlpha;
+    function ImageOutline(param) {
 
-    this.outLineColorRed = param.outlineColor.red;
-    this.outLineColorGreen = param.outlineColor.green;
-    this.outLineColorBlue = param.outlineColor.blue;
-    this.outLineColorAlpha = param.outlineColor.alpha;
+        this.img = param.img;
+        this.imgData = param.imgData;
+        this.searchAlphaPixel = param.searchAlphaPixel;
 
-    this.ctx = param.ctx;
+        this.color = param.color;
 
-    this.pixelPositions = [];
-}
+        this.pixelPositions = [];
+    }
 
-ImageOutline.prototype.draw = function () {
-    let this_o = this;
+    ImageOutline.prototype.getData = function () {
 
-    this.img.onload = function () {
+        var imgCount = this.imgData.data.length;
 
-        // нарисуем картинку для разбора
-        this_o.ctx.drawImage(this, this_o.imgX, this_o.imgY, this_o.imgW, this_o.imgH);
+        for (var i = 0; i < imgCount; i += 4) {
 
-        // Получим нарисованное изображение ввиде массива
-        let imgData = this_o.ctx.getImageData(this_o.imgX, this_o.imgY, this_o.imgW, this_o.imgH);
+            var red   = i,
+                green = i + 1,
+                blue  = i + 2,
+                alpha = i + 3;
 
-        let imgCount = imgData.data.length;
-        // let row = 0;
-        // let pixelCount = 0;
+            var alphaPixel = this.imgData.data[alpha];
 
-        for (let i = 0; i < imgCount; i += 4) {
-
-            // переход на новую строчку
-            // if (pixelCount === this_o.imgW) {
-            //     pixelCount = 0;
-            //     row++;
-            // }
-
-            // let red = imgData.data[i],
-            //     green = imgData.data[i + 1],
-            //     blue = imgData.data[i + 2],
-            //     alpha = imgData.data[i + 3];
-
-            // console.log('--row', row);
-            // console.log('--pixelCount', pixelCount);
-            // console.log('--index', i);
-            // console.log('--------------');
-
-            if (imgData.data[i + 3] === this_o.findAlpha) {
+            if (alphaPixel === this.searchAlphaPixel) {
 
                 // посмотрим на pixel вправо
-                let alphaPixelRight = this_o.checkSide(imgData.data, (i + 8));
+                var alphaPixelRight = this.checkSide(this.imgData.data, (i + 8));
 
                 // посмотрим на пиксель влево
-                let alphaPixelLeft = this_o.checkSide(imgData.data, (i - 8));
+                var alphaPixelLeft = this.checkSide(this.imgData.data, (i + 8));
 
                 // вычеслим такую же позицию пикселя только строчкой ниже
-                let line = this_o.imgW * 4;
+                var line = this.img.width * 4;
+
+                console.log(line);
+                console.log((line + 3));
+                console.log(i + (line + 3));
+                console.log('-------------');
 
                 // посмотрим на пиксель вниз
-                let alphaPixelBottom = this_o.checkSide(imgData.data, (i + (line + 3)));
+                var alphaPixelBottom = this.checkSide(this.imgData.data, (i + (line + 3)));
 
                 // посмотрим на пиксель вверх
-                let alphaPixelTop = this_o.checkSide(imgData.data, (i - (line - 3)));
+                var alphaPixelTop = this.checkSide(this.imgData.data, (i - (line - 3)));
 
                 if (
                     alphaPixelLeft
@@ -89,56 +68,66 @@ ImageOutline.prototype.draw = function () {
                     alphaPixelTop
                 ) {
 
-                    this_o.pixelPositions.push((i + 3));
+                    this.pixelPositions.push((i + 3));
 
-                    imgData.data[i] = this_o.outLineColorRed;
-                    imgData.data[i + 1] = this_o.outLineColorGreen;
-                    imgData.data[i + 2] = this_o.outLineColorBlue;
-                    imgData.data[i + 3] = this_o.outLineColorAlpha;
+                    this.imgData.data[red] = this.color.red;
+                    this.imgData.data[green] = this.color.green;
+                    this.imgData.data[blue] = this.color.blue;
+                    this.imgData.data[alpha] = 255;
 
                 }
             }
-
-            // pixelCount++;
         }
 
-        this_o.ctx.putImageData(imgData, 0, 0);
+        return this.imgData;
+
+    };
+
+    /**
+     * Проверим сторону
+     *
+     * @LastUpdate: 07.06.2018
+     *
+     * @param imgData
+     * @param index
+     * @returns {boolean}
+     */
+    ImageOutline.prototype.checkSide = function (imgData, index) {
+
+        if (index < 0) return false;
+
+        // посмотрим на pixel
+        var alphaPixel = imgData[index];
+
+        if (alphaPixel > 0) {
+            return this.isPixelNew(index);
+        }
+
+        return false;
+    };
+
+    /**
+     * Проверим, на наличе пикселя в массиве
+     *
+     * @LastUpdate: 07.06.2018
+     *
+     * @param i
+     * @returns {boolean}
+     */
+    ImageOutline.prototype.isPixelNew = function (i) {
+        var idx = this.pixelPositions.indexOf(i);
+
+        return idx === -1;
+    };
+
+    return {
+        init: function (param) {
+            if (!imageOutline) {
+                imageOutline = new ImageOutline(param);
+            }
+        },
+        getData: function () {
+            return imageOutline.getData();
+        }
     }
-};
-
-/**
- * Проверим сторону
- *
- * @LastUpdate: 07.06.2018
- *
- * @param imgData
- * @param index
- * @returns {*}
- */
-ImageOutline.prototype.checkSide = function (imgData, index) {
-
-    if (index < 0) return false;
-
-    // посмотрим на pixel
-    let alphaPixel = imgData[index];
-
-    if (alphaPixel > 0) {
-        return this.isPixelNew(index);
-    }
-
-    return false;
-};
-
-/**
- * Проверим, на наличе пикселя в массиве
- *
- * @LastUpdate: 07.06.2018
- *
- * @param i
- * @returns {boolean}
- */
-ImageOutline.prototype.isPixelNew = function (i) {
-    let idx = this.pixelPositions.indexOf(i);
-
-    return idx === -1;
-};
+}());
